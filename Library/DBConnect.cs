686bc20b606ae497bb3dc37008f594da1a60ce3b
h3d2b <ruchitpalrecha@hotmail.com>
2019-03-17 18:02:33 -0700

    * Library.csproj: * packages.config:
    * DBConnect.cs: Added DBConnect file

---
+++ Library/DBConnect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace Library
+{
+    public class DBConnect
+    {
+
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            server = "localhost";
+            database = "csharplibrary";
+            uid = "username";
+            password = "password";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+    }
+}
